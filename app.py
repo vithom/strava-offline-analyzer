4f25af69b804ef2502d2c62aed2b5e4018d0035a
vithom <74423292+vithom@users.noreply.github.com>
2025-11-05 23:14:23 +0100

    add template and Perspective widget

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
 import pandas as pd
 import panel as pn
 
+pn.extension('perspective')
+
 def get_data():
-    return pd.read_csv("data/export_171438604/activities.csv")
+    return pd.read_csv("data/export_171438604/activities.csv",
+                       parse_dates=['Activity Date']
+                       )
 
 df = get_data()
 
 df.info()
+
+# dff = df[["Activity Date", "Activity Name", "Activity Type", "Elapsed Time"]].groupby("Activity Type").sum("Elapsed Time")
+
+
+
+# component = pn.pane.panel(dff)
+
+pn.template.FastListTemplate(
+    title="Strava analyzer",
+    main = [pn.pane.Perspective(df, sizing_mode="stretch_both")],
+    accent = "orange"
+).servable()
