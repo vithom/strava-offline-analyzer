91c47b2fda23d169be918dca7b60e0c4bbd70512
Vincent THOMAS <vincent.thomas@volvo.com>
2025-12-10 18:25:37 +0100

    test with strava-client from swagger

--- app.py
+++ app.py
@@ -1,10 +1,43 @@
 import pandas as pd
 import panel as pn
+import plotly.graph_objs as go
+import locale
+import sys
+import os.path
+import pprint
 
-pn.extension('perspective')
+# import certifi
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'libs/swagger-python-client-generated')))
+# sys.path.append('libs/swagger-python-client-generated')
+
+# os.environ['SSL_CERT_FILE'] = certifi.where()
+# os.environ['CURL_CA_BUNDLE'] = ""
+# os.environ['REQUESTS_CA_BUNDLE'] = certifi.where()
+
+# print(sys.path)
+
+import strava_client
+from strava_client.rest import ApiException
+
+
+strava_client.configuration.access_token = "Bearer ed1aed442638afc20ad4c3e23a4f596a6d371749"
+
+api_instance = strava_client.ActivitiesApi()
+
+try:
+    res = api_instance.get_logged_in_athlete_activities()
+    pprint.pprint(res)
+except ApiException as e:
+    print("Exception raised: %s\n" % e)
+exit()
+
+
+pn.extension('perspective', 'plotly', 'echarts')
+
 
 def get_data():
-    return pd.read_csv("data/export_171438604/activities.csv",
+    return pd.read_csv("data/export_171438604_20251105/activities.csv",
                        parse_dates=['Activity Date']
                        )
 
@@ -12,14 +45,176 @@ df = get_data()
 
 df.info()
 
+# print(df.groupby(df["Activity Date"].dt.weekday)["Distance"].sum().reset_index().rename(columns={"Activity Date": "Month", "Distance": "Total Distance (km)"}))
+
 # dff = df[["Activity Date", "Activity Name", "Activity Type", "Elapsed Time"]].groupby("Activity Type").sum("Elapsed Time")
+
+#graphs
+# * Distance / activité / mois
 
 
 
 # component = pn.pane.panel(dff)
 
-pn.template.FastListTemplate(
+row = pn.Row(
+    pn.indicators.Number(
+        name="Total Activities",
+        value=len(df),
+        # format="{value} activities",
+        # sizing_mode="stretch_width"
+    ),
+    pn.indicators.Number(
+        name="Total Distance (km)",
+        value=round(df["Distance"].sum(), 2),
+        # format="{value} km",
+        # sizing_mode="stretch_width"
+    ),
+    pn.pane.Plotly(
+        df.groupby(pd.Grouper(key="Activity Date", freq="M"))["Distance"]
+            .sum()
+            .reset_index()
+            .pipe(
+            lambda d: {
+                "data": [
+                    {
+                        "x": d["Activity Date"],
+                        "y": d["Distance"],
+                        "type": "bar",
+                    }
+                ],
+                "layout": {
+                    "title": "Total Distance per Month",
+                    "xaxis": {"title": "Month"},
+                    "yaxis": {"title": "Total Distance (km)"},
+                },
+            }
+        ),
+        sizing_mode="stretch_width",
+    ),
+    pn.pane.Plotly(
+        df.groupby([pd.Grouper(key="Activity Date", freq="W-MON"), "Activity Type"])["Distance"]
+            .sum()
+            .reset_index()
+            .pivot(index="Activity Date", columns="Activity Type", values="Distance")
+            .fillna(0)
+            .pipe(
+                lambda pivot: {
+                    "data": [
+                        {
+                            # shift week labels one week earlier so each bar represents the week starting a week before
+                            "x": (pivot.index - pd.Timedelta(weeks=1)),
+                            "y": pivot[col].values,
+                            "type": "bar",
+                            "name": str(col),
+                        }
+                        for col in pivot.columns
+                    ],
+                    "layout": {
+                        "title": "Weekly Distance by Activity Type (Stacked)",
+                        "barmode": "stack",
+                        "xaxis": {"title": "Week"},
+                        "yaxis": {"title": "Total Distance (km)"},
+                        "legend": {"orientation": "h", "y": -0.2},
+                    },
+                }
+            ),
+        sizing_mode="stretch_width"
+    ),
+)
+
+df_months = df.groupby(pd.Grouper(key="Activity Date", freq="M"))["Distance"].sum()
+df_months2 = df.resample("1W", on="Activity Date", offset="1W").agg({"Distance": "sum", "Max Speed": "max"})
+
+row2 = pn.Row(
+    pn.pane.DataFrame(df_months),
+    pn.pane.DataFrame(df_months2),
+    # pn.pane.ECharts({
+    #     "tooltip": {
+    #         "trigger": 'axis',
+    #         "axisPointer": {
+    #             "type": 'shadow'
+    #         }
+    #     },
+    #     "xAxis": {
+    #         "data": df_months2.reset_index()['Activity Date'].apply(lambda x: x.strftime("%d %b %Y"))
+    #     },
+    #     "yAxis":{},
+    #     "series": [{
+    #         "type": "bar",
+    #         "data": df_months2['Distance'].values.tolist()
+    #     },
+    #     {
+    #         "type": "line",
+    #         "yAxisIndex": 1,
+    #         "data": df_months2['Max Speed'].values.tolist()
+    #     }],
+    #     "yAxis": [
+    #         {
+    #             "type": 'value',
+    #             "name": 'Distance (km)',
+    #         },
+    #         {
+    #             "type": 'value',
+    #             "name": 'Max Speed (km/h)',
+    #         }
+    #     ]
+    # # }, options={"opts": {"renderer":"svg"}
+    # }, sizing_mode="stretch_width", height=500)
+    pn.pane.ECharts(df.groupby([pd.Grouper(key="Activity Date", freq="W-MON"), "Activity Type"])["Distance"]
+            .sum()
+            .reset_index()
+            .pivot(index="Activity Date", columns="Activity Type", values="Distance")
+            .fillna(0)
+            .pipe(
+                lambda pivot: {
+                    "data": [
+                        {
+                            # shift week labels one week earlier so each bar represents the week starting a week before
+                            "x": (pivot.index - pd.Timedelta(weeks=1)),
+                            "y": pivot[col].values,
+                            "type": "bar",
+                            "name": str(col),
+                        }
+                        for col in pivot.columns
+                    ],
+                    "layout": {
+                        "title": "Weekly Distance by Activity Type (Stacked)",
+                        "barmode": "stack",
+                        "xaxis": {"title": "Week"},
+                        "yaxis": {"title": "Total Distance (km)"},
+                        "legend": {"orientation": "h", "y": -0.2},
+                    },
+                }
+            ), sizing_mode="stretch_width", height=500)
+)
+
+print(locale.getlocale())
+
+from time import strftime, gmtime
+
+print(strftime("%B %b", gmtime()))
+
+echart_bar = {
+    "tooltip": {
+        "trigger": 'axis',
+        "axisPointer": {
+            "type": 'shadow'
+        }
+    },
+    "xAxis": {
+        "data": df_months.reset_index()['Activity Date'].apply(lambda x: x.month_name(locale="fr") + x.strftime(" %Y"))
+    },
+    "yAxis":{},
+    "series": [{
+        "type": "bar",
+        "data": df_months.values.tolist()
+    }]
+}
+
+row3 = pn.Row(pn.pane.ECharts(echart_bar, options={"opts": {"renderer":"svg"}}, height=400, sizing_mode="stretch_width"))
+
+pn.template.VanillaTemplate(
     title="Strava analyzer",
-    main = [pn.pane.Perspective(df, sizing_mode="stretch_both")],
-    accent = "orange"
+    main = [row, row2, row3, pn.pane.Perspective(df, sizing_mode="stretch_both")],
+    # accent = "orange"
 ).servable()
